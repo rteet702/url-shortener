8429f182e61920677aad291d7b5e4a70cb71a8dd
Robert Teets <teetsrobert@gmail.com>
2023-02-16 18:16:04 -0700

    chore: inital commit and setup.

---
+++ README.md
@@ -0,0 +1,3 @@
+# URL Shortener
+
+## A project created by and maintained by Robert Teets.
---
+++ src/app/head.tsx
@@ -0,0 +1,16 @@
+export default function Head() {
+    return (
+        <>
+            <title>Trimmed - URL Shortener</title>
+            <meta
+                content="width=device-width, initial-scale=1"
+                name="viewport"
+            />
+            <meta
+                name="description"
+                content="URL Shortener created and maintained by Robert Teets."
+            />
+            <link rel="icon" href="/favicon.ico" />
+        </>
+    );
+}
